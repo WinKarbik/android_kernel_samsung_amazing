3e84dd50ede6170a63abf4667e62e7b29b4de874
WinKarbik <karbainovs@mail.ru>
2017-08-07 07:33:32 +0300

    amazing: Import osrc S6802XXLE1

---
+++ common/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 3 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 PREEMPT Fri May 4 14:48:42 KST 2012"
+#define LINUX_COMPILE_TIME "14:48:42"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "dspark-desktop"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
